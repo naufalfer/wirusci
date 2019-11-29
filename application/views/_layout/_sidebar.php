01812b23aae0276fda052245b1465b97c57a8fe6
naufal <naufal.ferdy.tif17@polban.ac.id>
2019-11-29 18:48:24 +0700

    Add filter sidebar by statusid

--- application/views/_layout/_sidebar.php
+++ application/views/_layout/_sidebar.php
@@ -14,21 +14,24 @@
         </a>
       </li>
 
-      <li <?php if ($userdata->roleid == '1' OR $userdata->roleid == '4' OR $userdata->roleid == '3') { ?>>
+      <li <?php  $user = $this->session->userdata();
+      if (($userdata->roleid == '1' OR $userdata->roleid == '4' OR $userdata->roleid == '3') AND $user['userdata']->statusid == 0 ){ ?>>
         <a href="<?php echo base_url('Pertanyaan'); ?>">
           <i class="fa fa-user"></i>
           <span>Pengajuan Tahap 1</span>
         </a>
       </li> <?php } ?>
 
-      <li <?php if ($userdata->roleid == '1' OR $userdata->roleid == '4' OR $userdata->roleid == '3'){ ?>>
+      <li <?php $user = $this->session->userdata();
+      if (($userdata->roleid == '1' OR $userdata->roleid == '4' OR $userdata->roleid == '3') AND $user['userdata']->statusid == 1 ){ ?>>
         <a href="<?php echo base_url('Posisi'); ?>">
           <i class="fa fa-user"></i>
           <span>Pengajuan Tahap 2</span>
         </a>
       </li> <?php } ?>
 
-      <li <?php if ($userdata->roleid == '1' OR $userdata->roleid == '4' OR $userdata->roleid == '3'){ ?>>
+      <li <?php $user = $this->session->userdata();
+      if (($userdata->roleid == '1' OR $userdata->roleid == '4' OR $userdata->roleid == '3') AND $user['userdata']->statusid == 2 ){ ?>>
         <a href="<?php echo base_url('Posisi/tahap_3'); ?>">
           <i class="fa fa-user"></i>
           <span>Pengajuan Tahap 3</span>
